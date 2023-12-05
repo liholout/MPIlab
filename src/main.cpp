1c9c386988c5b35e07adedc984daa8e85c0c2920
Polly <99385021+liholout@users.noreply.github.com>
2023-12-05 21:16:33 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,59 @@
+#include <iostream>
+#include <mpi.h>
+#include <vector>
+#include <string>
 
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+
+    int world_size, world_rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    std::vector<std::string> menu = {"Паста", "Пицца", "Суп", "Салат", "Стейк"};
+  
+    // Повар
+    if (world_rank == 0) {
+        
+        int students = 15;
+
+        for (int i = 0; i < students * 2; ++i) {
+            MPI_Request request;
+            MPI_Status status;
+            std::string dish;
+
+            // Принимаем заказ от студента
+            MPI_Irecv(&dish[0], dish.size(), MPI_CHAR, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &request);
+            MPI_Wait(&request, &status);
+            std::cout << "Повар принял заказ от студента " << status.MPI_SOURCE << ": " << dish << std::endl;
+
+            // Готовим блюдо
+            usleep(100000); // Имитация приготовления блюда
+
+            // Выдаем блюдо студенту
+            MPI_Isend(&dish[0], dish.size() + 1, MPI_CHAR, status.MPI_SOURCE, 0, MPI_COMM_WORLD, &request);
+            MPI_Wait(&request, MPI_STATUS_IGNORE);
+            std::cout << "Повар выдал блюдо студенту " << status.MPI_SOURCE << ": " << dish << std::endl;
+        }
+    }
+    // Студенты
+    else {
+        for (int i = 0; i < 2; ++i) {
+            MPI_Request request;
+            std::string dish = menu % ;  // Произвольное начальное блюдо
+
+            // Отправляем заказ повару
+            MPI_Isend(&dish[0], dish.size() + 1, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &request);
+            MPI_Wait(&request, MPI_STATUS_IGNORE);
+            std::cout << "Студент " << world_rank << " отправил заказ повару: " << dish << std::endl;
+
+            // Принимаем блюдо от повара
+            MPI_Irecv(&dish[0], dish.size(), MPI_CHAR, 0, 0, MPI_COMM_WORLD, &request);
+            MPI_Wait(&request, MPI_STATUS_IGNORE);
+            std::cout << "Студент " << world_rank << " получил блюдо от повара: " << dish << std::endl;
+        }
+    }
+
+    MPI_Finalize();
+    return 0;
+}
