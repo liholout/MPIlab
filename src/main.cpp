35bc6451f6b6ee13189cce79b75a25899de324bb
Polly <99385021+liholout@users.noreply.github.com>
2023-12-05 21:38:27 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <string>
 #include <unistd.h>
+#include <random>
+
 
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
@@ -22,7 +24,7 @@ int main(int argc, char** argv) {
             std::string dish;
 
             // Принимаем заказ от студента
-            MPI_Irecv(&dish[0], dish.size() + 1, MPI_CHAR, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &request);
+            MPI_Irecv(&dish, dish.size() + 1, MPI_CHAR, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &request);
             MPI_Wait(&request, &status);
             std::cout << "Повар принял заказ от студента " << status.MPI_SOURCE << ": " << dish << std::endl;
 
@@ -30,7 +32,7 @@ int main(int argc, char** argv) {
             usleep(100000); // Имитация приготовления блюда
 
             // Выдаем блюдо студенту
-            MPI_Isend(&dish[0], dish.size() + 1, MPI_CHAR, status.MPI_SOURCE, 0, MPI_COMM_WORLD, &request);
+            MPI_Isend(&dish[0, dish.size() + 1, MPI_CHAR, status.MPI_SOURCE, 0, MPI_COMM_WORLD, &request);
             MPI_Wait(&request, MPI_STATUS_IGNORE);
             std::cout << "Повар выдал блюдо студенту " << status.MPI_SOURCE << ": " << dish << std::endl;
         }
@@ -38,17 +40,20 @@ int main(int argc, char** argv) {
     // Студенты
     else {
         for (int i = 0; i < 2; ++i) {
+            std::mt19937 rng(std::random_device{}()); 
+            srand(static_cast<unsigned int>(time(nullptr)));
             MPI_Request request;
             std::vector<std::string> menu = {"Паста", "Пицца", "Суп", "Салат", "Стейк"};
-            std::string dish = menu[rand() % menu.size()];  // Выбираем случайное блюдо
+            std::string dish = menu[rng() % menu.size()];  // Выбираем случайное блюдо
 
             // Отправляем заказ повару
-            MPI_Isend(&dish[0], dish.size() + 1, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &request);
+            MPI_Isend(&dish, dish.size() + 1, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &request);
             MPI_Wait(&request, MPI_STATUS_IGNORE);
             std::cout << "Студент " << world_rank << " отправил заказ повару: " << dish << std::endl;
 
+            dish = menu[rng() % menu.size()];  // Выбираем случайное блюдо
             // Принимаем блюдо от повара
-            MPI_Irecv(&dish[0], dish.size() + 1, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &request);
+            MPI_Irecv(&dish, dish.size() + 1, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &request);
             MPI_Wait(&request, MPI_STATUS_IGNORE);
             std::cout << "Студент " << world_rank << " получил блюдо от повара: " << dish << std::endl;
         }
